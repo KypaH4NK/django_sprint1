53545712bd9a3cc1db960578d174e9d704776771
Кирилл Куранов <kypahob11@gmail.com>
2023-08-04 17:43:36 +0300

    Fixed tests

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -7,6 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_slug>/',
-         views.category_post, name='category_slug'),
+         views.category_post, name='category_posts'),
     path('', views.index, name='index'),
 ]
